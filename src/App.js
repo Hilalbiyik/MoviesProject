893b48af52efc9632aaaabb2523dac5675af966d
Hilalbiyik <hilalbyk396@gmail.com>
2022-11-02 00:29:50 +0300

    React Router Dom has been used and updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import Navbar from './components/Navbar/Navbar';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Movies from "./pages/Movies/Movies";
+import Home from "./pages/Home/Home";
+import Series from "./pages/Series/Series";
+import Trends from "./pages/Trends/Trends";
 
 function App() {
+
   return (
     <div className="App">
-      <Navbar/>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route  path="/movies" component={Movies} />
+          <Route  path="/series" component={Series} />
+          <Route  path="/trends" component={Trends} />
+        </Switch>
+      </Router>
     </div>
   );
 }
-
 export default App;
