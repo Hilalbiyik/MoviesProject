009b66baadf4aa813a41cb57476beae4ac90a5e6
Hilalbiyik <hilalbyk396@gmail.com>
2022-11-02 00:26:36 +0300

    Movies Page has been created

---
+++ src/pages/Movies/Movies.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+
+const Movies = () => {
+  return (
+    <div>
+      <Navbar />
+      Movies Page
+    </div>
+  );
+};
+
+export default Movies;
