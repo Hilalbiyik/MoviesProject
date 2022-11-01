95360b4b5ce2b714ccc666bd03f82f9792bd8332
Hilalbiyik <hilalbyk396@gmail.com>
2022-11-02 00:25:53 +0300

    Trends Page has been created

---
+++ src/pages/Trends/Trends.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+
+const Trends = () => {
+  return (
+    <div>
+      <Navbar />
+      Trends Page
+    </div>
+  );
+};
+
+export default Trends;
